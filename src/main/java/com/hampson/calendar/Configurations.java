bd4fd9ecaf7d30a475b8cd4c262719b403b9df0a
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-01-03 16:56:38 -0500

    Add files via upload

---
+++ src/main/java/com/hampson/calendar/Configurations.java
@@ -0,0 +1,43 @@
+package com.hampson.calendar;
+
+import org.apache.commons.configuration.XMLPropertiesConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import java.util.Scanner;
+import java.io.File;
+
+public class Configurations {
+	
+	private static final XMLPropertiesConfiguration configs = getXMLConfigurations();
+
+	public static final String CLIENT_ID = getProperty("clientId", configs);
+	public static final String CLIENT_SECRET = getProperty("clientSecret", configs);
+	public static final String REDIRECT_URI = getProperty("redirectUri", configs);
+	public static final String APP_NAME = getProperty("appName", configs);
+
+	private static String getProperty(String name, XMLPropertiesConfiguration configs) {
+		if (configs.containsKey(name))
+			return (String) configs.getProperty(name);
+		else {
+			Scanner in = new Scanner(System.in);
+			System.out.println("Set property '" + name + "':");
+			String value = in.nextLine();
+			configs.addProperty(name, value);
+			try {
+				configs.save();
+			} catch (ConfigurationException ce) {
+				throw new RuntimeException("Could not save configuration: " + name);
+			} finally {
+				in.close();
+			}
+			return value;
+		}
+	}
+	
+	private static XMLPropertiesConfiguration getXMLConfigurations() {
+		try {
+			return new XMLPropertiesConfiguration(new File("config.xml"));
+		} catch (ConfigurationException ce) {
+			throw new RuntimeException("Error initializing configuration file");
+		}
+	}
+}
