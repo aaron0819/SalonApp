ffdb833ec44d104a57b5288c7651a942d61ad04d
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-01-02 21:10:37 -0500

    Add files via upload

--- src/main/java/com/hampson/dao/AppointmentDAO.java
+++ src/main/java/com/hampson/dao/AppointmentDAO.java
@@ -15,7 +15,7 @@ public class AppointmentDAO {
 	public List<Map<String, Object>> getAllAppointments() {
 		List<Map<String, Object>> appointments = new ArrayList<Map<String, Object>>();
 
-		String sql = "SELECT * from appointment";
+		String sql = "select appointment.id, appointment.stylist_id, appointment.time, customer.first_name, customer.last_name, customer.phone_num from appointment, customer where (appointment.customer_id = customer.id)";
 
 		List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
 		for (Map<String, Object> row : list) {
