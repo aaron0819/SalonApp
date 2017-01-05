2359282b2df6bc9f484b425f94c47ef9be55b77d
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-01-04 20:40:36 -0500

    Add files via upload

--- src/main/java/com/hampson/controller/AuthenticatorController.java
+++ src/main/java/com/hampson/controller/AuthenticatorController.java
@@ -135,6 +135,7 @@ public class AuthenticatorController {
 
 	private String parseDate(String date) {
 		date = date.substring(date.indexOf(":") + 2, date.lastIndexOf("T"));
+		System.out.println(date);
 		
 		LocalDateTime datetime = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 		System.out.println("A: " + datetime.getMonth());
