9b62d7d966b52497f972b796e2296b62c08f7417
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-01-04 22:20:24 -0500

    Add files via upload

--- src/main/java/com/hampson/controller/AuthenticatorController.java
+++ src/main/java/com/hampson/controller/AuthenticatorController.java
@@ -135,9 +135,9 @@ public class AuthenticatorController {
 
 	private String parseDate(String date) {
 		date = date.substring(date.indexOf(":") + 2, date.lastIndexOf("T"));
-		int day = Integer.parseInt(date.substring(0, date.indexOf("-")));
-		int month = Integer.parseInt(date.substring(date.indexOf("-"), date.lastIndexOf("-")));
-		int year = Integer.parseInt(date.substring(date.lastIndexOf("-")));
+		int year = Integer.parseInt(date.substring(0, date.indexOf("-")));
+		int month = Integer.parseInt(date.substring(date.indexOf("-") + 1, date.lastIndexOf("-")));
+		int day = Integer.parseInt(date.substring(date.lastIndexOf("-") + 1));
 		
 		return Integer.toString(day) + " " + Integer.toString(month) + " " + Integer.toString(year);
 	}
