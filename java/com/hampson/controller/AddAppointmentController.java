453f8e0e07616c46928902f05992b62017a14dd5
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-01-12 21:12:36 -0500

    Add files via upload

--- java/com/hampson/controller/AddAppointmentController.java
+++ java/com/hampson/controller/AddAppointmentController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
@@ -19,7 +20,7 @@ import com.google.api.services.calendar.model.EventDateTime;
 public class AddAppointmentController {
 
 	@RequestMapping("/addAppointment")
-	public String addEvent(HttpServletRequest request,
+	public ModelAndView addEvent(HttpServletRequest request,
 			@RequestParam("appointmentType") String appointmentType,
 			@RequestParam("customerFirstName") String customerFirstName,
 			@RequestParam("customerLastName") String customerLastName,
@@ -44,6 +45,6 @@ public class AddAppointmentController {
 		Calendar calendar = (Calendar) request.getSession().getAttribute("calendar");
 		event = calendar.events().insert(calendarId, event).execute();
 				
-		return "successfulAppointmentAdd";
+		return new ModelAndView("redirect:/oauth2callback");
 	}
 }
