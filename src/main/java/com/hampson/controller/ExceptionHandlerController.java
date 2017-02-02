1f8fc0d11a921daaa9a028c9d088ed92683d371a
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-02-02 18:24:14 -0500

    Add files via upload

--- src/main/java/com/hampson/controller/ExceptionHandlerController.java
+++ src/main/java/com/hampson/controller/ExceptionHandlerController.java
@@ -3,6 +3,7 @@ package com.hampson.controller;
 import static java.time.LocalDateTime.now;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,11 +15,11 @@ public class ExceptionHandlerController {
 	public static final String DEFAULT_ERROR_VIEW = "error";
 
 	@ExceptionHandler(value = { Exception.class, RuntimeException.class })
-	public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) {
+	public ModelAndView defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
 		ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
 
 		mav.addObject("datetime", now());
-		mav.addObject("exception", e.getMessage() + "\n\n" + e.getStackTrace());
+		mav.addObject("exception", e.getMessage() + "\n\n" + e.getStackTrace() + "\n\nSTATUS CODE: " + response.getStatus());
 		mav.addObject("url", request.getRequestURL());
 
 		System.out.println("ERROR: " + now() + "\n" + e.getMessage() + "\n\n" + e.getStackTrace());
