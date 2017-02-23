cb18f4bb58223cf39ec4aa409eb91b7181c87d3c
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-02-22 19:10:58 -0500

    Add files via upload

--- src/main/java/com/hampson/model/Customer.java
+++ src/main/java/com/hampson/model/Customer.java
@@ -2,30 +2,13 @@ package com.hampson.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "Customers")
 public class Customer implements Serializable {
 
 	private static final long serialVersionUID = 2822027024447584056L;
 
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
-	 
-	@Column(name = "first_name")
 	private String firstName;
-	
-	@Column(name = "last_name")
 	private String lastName;
-	
-	@Column(name = "phone_number")
 	private String phoneNumber;
 
 	protected Customer() {
